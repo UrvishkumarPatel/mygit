b2755f6268260e06301fbde97dc98d238ceb04bb
Urvishkumar Patel <patel.urvishkumar@iitgn.ac.in>
2020-11-06 19:42:36 +0530

    branch completed

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include "commit.h"
+#include "branch.h"
 
 using namespace std;
 
@@ -24,6 +25,7 @@ int main(int argc, char* argv[]){
 	else if(strcmp(argv[1],"init")==0){ init(argc,argv);}  //calling init
 	else if(strcmp(argv[1],"add")==0){ add(argc,argv); } //calling add
 	else if(strcmp(argv[1],"commit")==0){commit(argc,argv); } //calling commit
+	else if(strcmp(argv[1],"branch")==0){branch(argc,argv); } //calling branch
 	else if(strcmp(argv[1],"exit")==0) exit(1); //exiting the function
     return 0;
 }
